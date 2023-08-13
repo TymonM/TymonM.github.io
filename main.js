8438913b3229133d3cfcf15ca1fc5deb071f90fd
tymon <tymon.mieszkowski@gmail.com>
2023-08-13 20:58:45 +1200

    much-needed refactoring js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let score = {
     Dark: 0
 }
 
-let moveFrom = null;
+let highlighted = null;
 let phase = 1; // 1: placing, 0: moving
 
 const cells = Array.from(document.querySelectorAll('.cell'));
@@ -19,59 +19,6 @@ function updateUI() {
 
     updatePhase();
 }
-
-cells.forEach((cell, i) => {
-    cell.addEventListener('click', () => {
-        if (board[i] === null && moveFrom !== null) {
-            if (!isLegalMove(moveFrom, i)) {
-                cells[i].classList.add('illegal');
-                setTimeout(() => { cells[i].classList.remove('illegal') }, 300)
-                return;
-            }
-            // If the cell clicked has no content and they are moving a piece
-            // Reset moveFrom cell
-            cells[moveFrom].classList.remove('highlight');
-            cells[moveFrom].classList.remove(currentPlayer);
-            board[moveFrom] = null;
-
-            // Set new cell
-            board[i] = currentPlayer;
-            cells[i].classList.add(currentPlayer);
-
-            // Toggle player
-            currentPlayer = currentPlayer === 'Light' ? 'Dark' : 'Light';
-
-            // Reset moveFrom
-            moveFrom = null;
-        } else if (board[i] === null && phase === 1) {
-            // If they are placing a piece and the cell clicked has no content
-            // set the new piece
-            board[i] = currentPlayer;
-            cell.classList.add(currentPlayer);
-
-            // Toggle player
-            currentPlayer = currentPlayer === 'Light' ? 'Dark' : 'Light';
-
-            // Check if the placing phase is over or not, if it is, modify 
-            if (board.filter(x => x !== null).length >= 6) {
-                phase = 0;
-            }
-        } else if (board[i] === currentPlayer && phase === 0) {
-            // If they are selecting which piece to move
-            if (moveFrom !== null) {
-                cells[moveFrom].classList.remove('highlight');
-            }
-            moveFrom = i;
-            cell.classList.add('highlight');
-        }
-        updateUI();
-        if (checkWin()) {
-            score[currentPlayer == 'Light' ? 'Dark' : 'Light']++;
-            showWin();
-            // location.reload();
-        }
-    });
-});
 
 function isLegalMove(a, b) {
     return (a === 4 || b === 4 || (a % 3 !== 0 && b === a - 1) || (a % 3 !== 2 && b === a + 1) || (a >= 3 && b === a - 3) || (a < 6 && b === a + 3));
@@ -116,77 +63,83 @@ function spiral(index) {
 }
 
 function initialise() {
-    board = Array(9).fill(null);
-
-    // Randomize starting player
-    let options = ['Light', 'Dark'];
-    currentPlayer = options[Math.floor(Math.random() * options.length)];
-
-    moveFrom = null;
-    phase = 1; // 1: placing, 0: moving
-}
-
-function start() {
-    initialise();
-    updateUI();
-
     cells.forEach((cell, i) => {
         cell.addEventListener('click', () => {
-            if (board[i] === null && moveFrom !== null) {
-                if (!isLegalMove(moveFrom, i)) {
-                    cells[i].classList.add('illegal');
-                    setTimeout(() => { cells[i].classList.remove('illegal') }, 300)
-                    return;
-                }
-                // If the cell clicked has no content and they are moving a piece
-                // Reset moveFrom cell
-                cells[moveFrom].classList.remove('highlight');
-                cells[moveFrom].classList.remove(currentPlayer);
-                board[moveFrom] = null;
-
-                // Set new cell
-                board[i] = currentPlayer;
-                cells[i].classList.add(currentPlayer);
-
-                // Toggle player
-                currentPlayer = currentPlayer === 'Light' ? 'Dark' : 'Light';
-
-                // Reset moveFrom
-                moveFrom = null;
+            if (board[i] === null && highlighted !== null) {
+                movePieceTo(i);
             } else if (board[i] === null && phase === 1) {
-                // If they are placing a piece and the cell clicked has no content
-                // set the new piece
-                board[i] = currentPlayer;
-                cell.classList.add(currentPlayer);
-
-                // Toggle player
-                currentPlayer = currentPlayer === 'Light' ? 'Dark' : 'Light';
-
-                // Check if the placing phase is over or not, if it is, modify 
-                if (board.filter(x => x !== null).length >= 6) {
-                    phase = 0;
-                    alert("Placing phase over, take turns moving pieces");
-                }
+                placePiece(i, cell);
             } else if (board[i] === currentPlayer && phase === 0) {
-                // If they are selecting which piece to move
-                if (moveFrom !== null) {
-                    cells[moveFrom].classList.remove('highlight');
-                }
-                moveFrom = i;
-                cell.classList.add('highlight');
+                highlightCell(i, cell);
             }
 
-            updateUI();
-
             if (checkWin()) {
+                score[currentPlayer == 'Light' ? 'Dark' : 'Light']++;
                 showWin();
                 setTimeout(() => {
                     animateClear();
                     start();
                 }, 2500)
             }
+            updateUI();
         });
     });
+}
+
+function start() {
+    board = Array(9).fill(null);
+
+    // Randomize starting player
+    let options = ['Light', 'Dark'];
+    currentPlayer = options[Math.floor(Math.random() * options.length)];
+
+    highlighted = null;
+    phase = 1; // 1: placing, 0: moving
+
+    updateUI();
+}
+
+function movePieceTo(i) {
+    if (!isLegalMove(highlighted, i)) {
+        cells[i].classList.add('illegal');
+        setTimeout(() => { cells[i].classList.remove('illegal') }, 300)
+        return;
+    }
+    // Reset highlighted cell
+    cells[highlighted].classList.remove('highlight');
+    cells[highlighted].classList.remove(currentPlayer);
+    board[highlighted] = null;
+
+    // Set new cell
+    board[i] = currentPlayer;
+    cells[i].classList.add(currentPlayer);
+
+    // Toggle player
+    currentPlayer = currentPlayer === 'Light' ? 'Dark' : 'Light';
+
+    // Reset highlighted
+    highlighted = null;
+}
+
+function placePiece(i, cell) {
+    board[i] = currentPlayer;
+    cell.classList.add(currentPlayer);
+
+    // Toggle player
+    currentPlayer = currentPlayer === 'Light' ? 'Dark' : 'Light';
+
+    // Check if the placing phase is over or not, if it is, modify 
+    if (board.filter(x => x !== null).length >= 6) {
+        phase = 0;
+    }
+}
+
+function highlightCell(i, cell) {
+    if (highlighted !== null) {
+        cells[highlighted].classList.remove('highlight');
+    }
+    highlighted = i;
+    cell.classList.add('highlight');
 }
 
 function animateClear() {
@@ -214,4 +167,5 @@ function changeColour(colour) {
     document.documentElement.style.setProperty(`--${currentPlayer}-colour`, colour);
 }
 
+initialise();
 start();
